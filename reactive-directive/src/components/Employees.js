4ebb99a07c12557292fe372fbe36f16a5c3caea0
Bohdan Pechenyak <bohdan.pechenyak@gmail.com>
2021-01-20 14:46:12 -0500

    updgraded styling and presentation of the employee info

--- reactive-directive/src/components/Employees.js
+++ reactive-directive/src/components/Employees.js
@@ -17,7 +17,16 @@ export default class Employees extends React.Component {
 
   render() {
     return (
+    <div className="container">
+      <header>
+        <h1 className="center">Employee Information</h1>
+        <nav>
+          &nbsp;
+        </nav>
+      </header>
+      <main>
       <table>
+        <thead>
         <tr className="center">
           <th>Photo</th>
           <th className="narrow">Title</th>
@@ -26,32 +35,41 @@ export default class Employees extends React.Component {
           <th className="bg-fill narrow">Yrs <br/> Empl.</th>
           <th className="narrow">Age</th>
           <th className="bg-fill narrow">Gender</th>
+          <th className="narrow">Time Zone,<br/>GMT+/-</th>
+          <th className="bg-fill indent">Address</th>
           <th>Phone</th>
           <th className="bg-fill">Email</th>
-          <th>Time Zone <br/> (GMT+/-)</th>
-          <th className="bg-fill indent">Address</th>
+
         </tr>
+        </thead>
+        <tbody>
           {this.state.employees
             .map(employee => { 
               return(
-              <tr className="center">
-                <td className="wide"><img src={employee.picture.medium} alt="Employee Photo"/></td>
-                <td className="center">{employee.name.title} <br/><br/><br/></td>
+              <tr className="center" key={employee.login.uuid}>
+                <td className="wide"><img src={employee.picture.medium} alt="Employee"/></td>
+                <td className="center narrow">{employee.name.title} <br/><br/><br/></td>
                 <td className="bg-fill">{employee.name.first} <br/><br/><br/></td>
                 <td>{employee.name.last} <br/><br/><br/></td>
-                <td className="bg-fill center">{employee.registered.age} <br/><br/><br/></td>
-                <td>{employee.dob.age} <br/><br/><br/></td>
-                <td className="bg-fill center">{employee.gender[0].toUpperCase()} <br/><br/><br/></td>
-                <td>{employee.cell} <br/><br/><br/></td>
-                <td className="bg-fill">{employee.email} <br/><br/><br/></td>
+                <td className="bg-fill center narrow">{employee.registered.age} <br/><br/><br/></td>
+                <td className="narrow">{employee.dob.age} <br/><br/><br/></td>
+                <td className="bg-fill center narrow">{employee.gender[0].toUpperCase()} <br/><br/><br/></td>
                 <td className="center">{employee.location.timezone.offset} <br/><br/><br/></td>
                 <td className="bg-fill indent">{employee.location.street.number} {employee.location.street.name} <br/> {employee.location.city}, {employee.location.state || null} <br/> {employee.location.country}, {employee.location.postcode}</td>
+                <td>{employee.cell} <br/><br/><br/></td>
+                <td className="bg-fill">{employee.email} <br/><br/><br/></td>
+
               </tr>
               )
             })
           }
+        </tbody>
       </table>
-     
+      </main>
+      <footer>
+        &nbsp;
+      </footer>
+    </div>
     )
   }
 }
