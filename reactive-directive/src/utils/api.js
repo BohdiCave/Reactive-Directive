bcfc8fdebad8d2cad045b3d9824d37c9450766bb
Bohdan Pechenyak <bohdan.pechenyak@gmail.com>
2021-01-23 01:27:09 -0500

    removing unnecessary files

--- reactive-directive/src/utils/api.js
+++
@@ -1,8 +0,0 @@
-
-
-// Given a table of random users generated from the Random User API, 
-// when the user loads the page, a table of employees should render.
-// 
-// The user should be able to:
-// * Sort the table by at least one category
-// * Filter the users by at least one property.
